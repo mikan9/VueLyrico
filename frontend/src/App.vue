aebea213c781aec8188357ff201500875dfe77cd
Stephanie Ahlström <ahlstrom.stephanie.se@gmail.com>
2021-09-30 00:54:30 +0200

    Use vue router router-view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,11 @@
 <template>
-  <Home />
+	<div id="app">
+		<router-view />
+	</div>
 </template>
 
 <script>
-import Home from "@/views/Home";
 export default {
-  name: "App",
-  components: { Home },
+	name: "App",
 };
 </script>
-
-<style>
-</style>
