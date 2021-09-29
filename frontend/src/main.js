08a86c4cea828878c5da52b80d4610804348deac
Stephanie Ahlström <ahlstrom.stephanie.se@gmail.com>
2021-09-30 00:56:01 +0200

    Import modules

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,29 +1,6 @@
 window._ = require('lodash');
 
 /**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap');
-} catch (e) { }
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
@@ -42,6 +19,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.Vue = require('vue').default;
 
+import axios from 'axios';
+Vue.prototype.$http = axios;
+
 import { BootstrapVue } from 'bootstrap-vue';
 import '@/assets/scss/app.scss';
 Vue.use(BootstrapVue);
@@ -51,7 +31,17 @@ import App from './App.vue';
 import VueCompositionApi from '@vue/composition-api';
 Vue.use(VueCompositionApi);
 
+import store from '@/store';
+
+import router from '@/router';
+
+import i18n from '@/libs/i18n';
+import Vue from 'vue';
+
 const app = new Vue({
-    el: '#app',
-    render(h) { return h(App) }
-});
+    i18n,
+    store,
+    router,
+    components: { App },
+    template: '<App />',
+}).$mount('#app');
